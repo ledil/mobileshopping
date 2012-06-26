3b7009157c33e4cc351fe5b40faaf7cb1b9a3a96
Leonardo Di Lella <leonardo.dilella@mobileapart.com>
2012-06-26 09:22:59 +0200

    initial import

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+	var w3cnavjs=document.createElement('SCRIPT');
+	w3cnavjs.type='text/javascript';
+	w3cnavjs.src='https://raw.github.com/ledil/mobileshopping/master/mobileshopping.js';
+	document.getElementsByTagName('head')[0].appendChild(w3cnavjs);
+})();
