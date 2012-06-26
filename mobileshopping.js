ced357c1d9b0157720c36d0d628ffb0890fcb261
Leonardo Di Lella <leonardo.dilella@mobileapart.com>
2012-06-26 09:52:55 +0200

    .

--- mobileshopping.js
+++ mobileshopping.js
@@ -14,16 +14,22 @@
 	waterfall([
 		// Load jQuery
 		function(next) {
-		  if(window.jQuery) {
-			//next(null);
-			console.log("japp, da");
-		  } else {
-			console.log("huhu");
-			var script = document.createElement("script");
-			script.src = "http://code.jquery.com/jquery-1.7.1.min.js";
-			script.onload = next;
-			document.getElementsByTagName("head")[0].appendChild(script);
-		  }
+			if (typeof(jQuery) != "undefined") {
+				console.log("japp, da");
+				test2 = next;
+				next(null);
+			} else {
+				console.log("huhu");
+				var script = document.createElement("script");
+				script.src = "http://code.jquery.com/jquery-1.7.1.min.js";
+				script.onload = next;
+				document.getElementsByTagName("head")[0].appendChild(script);
+			}
 		},
+		// create Iframe
+		function(next) {
+			test = next;
+			console.log("test");
+		}
 	]);
 })(window);  
